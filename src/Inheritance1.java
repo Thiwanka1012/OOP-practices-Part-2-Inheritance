481c8d37703611d5dc13d1f6a6c906954eadb6f6
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-12-22 22:41:13 +0530

    J Frames

--- src/Inheritance1.java
+++ src/Inheritance1.java
@@ -6,14 +6,19 @@ class Calculator extends JFrame {   //Inherit
     private JButton yesButton;  //Attribute,Reference Variable
     private JButton noButton;
 
+    private JButton enterButton;
+
     Calculator(){
         setSize(400,400);
         setTitle("Calculator");
         yesButton=new JButton("Yes");
-        add(yesButton);
+        add("North",yesButton);
+
+        enterButton=new JButton("Enter");
+        add("Center",enterButton);
 
         noButton=new JButton("NO");
-        add(noButton);
+        add("South",noButton);
 
 
 
