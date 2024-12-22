fe5494d3dff237a3408d620659406899005a311f
Thiwanka1012 <160256984+Thiwanka1012@users.noreply.github.com>
2024-12-22 22:31:41 +0530

    njnjn

--- src/Inheritance1.java
+++ src/Inheritance1.java
@@ -3,13 +3,19 @@ import javax.swing.*;
 class Calculator extends JFrame {   //Inherit
 
 
-    private JButton existButton;   //Attribute,Reference Variable
+    private JButton yesButton;  //Attribute,Reference Variable
+    private JButton noButton;
 
     Calculator(){
         setSize(400,400);
         setTitle("Calculator");
-        existButton=new JButton("Exist Button");
-        add(existButton);
+        yesButton=new JButton("Yes");
+        add(yesButton);
+
+        noButton=new JButton("NO");
+        add(noButton);
+
+
 
     }
 }
@@ -36,6 +42,8 @@ public class Inheritance1 {
     public static void main(String[] args) {
 
         Calculator c=new Calculator();
+        c.setSize(400,500);
+        c.setTitle("Cal");
         c.setVisible(true);
     }
 }
